1042bc9e9589e4df7576c3fa4d4a7892ba550589
Asaph Barros <asaphbarros@gmail.com>
2021-12-26 13:20:33 -0300

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Asaph Barros on 26/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
